83fdff557bbfeaeec338b2df05e0dd1fc9c2ce78
Thuruv <Thuruv@users.noreply.github.com>
2015-06-02 23:11:34 +0530

    Update apod_scrapper.py

--- apod_scrapper.py
+++ apod_scrapper.py
@@ -5,15 +5,21 @@ import os
 
 url = 'http://apod.nasa.gov/apod/'
 soup = BeautifulSoup(urllib.urlopen(url).read())
-temp = soup.img['src']
-myPath = # your file path
-
-full_url = url + temp
-title = (soup.body.b.text).strip('\n') + '.jpg'
-nasa = os.path.join(myPath, title)
-urllib.urlretrieve(full_url,nasa)
-details =  (title , strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-
-with open('/mypath/apod_imagedb.txt','wb+') as f:
-    f.write(str(details))
-    f.close()
+try:
+    temp = soup.img['src']
+    myPath = # your Path in drive
+    data = []
+    full_url = url + temp
+    title = (soup.body.b.text).strip('\n') + '.jpg'
+    nasa = os.path.join(myPath, title)
+    urllib.urlretrieve(full_url,nasa)
+    details =  data.append((title , strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+    with open('./apod_imagedb.txt','wb+') as f:
+        f.write(str(data))
+        f.close()
+except TypeError:
+    res =  'They Has No Image For Today. .! '
+    print res
+    with open('./apod_imagedb.txt','wb+') as f:
+        f.write(str(res))
+        f.close()
